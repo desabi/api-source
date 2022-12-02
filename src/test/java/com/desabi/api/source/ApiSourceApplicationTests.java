97448a8d11568a4dd4292c5ede04042c6feab2f8
abigahil.delgado <abigahil.delgado@ine.mx>
2022-12-02 12:43:28 -0600

    feat: se agrega proyecto base

---
+++ src/test/java/com/desabi/api/source/ApiSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.api.source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
